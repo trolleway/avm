15d0e7a7fbd8a05b377e0e2c46735a59af6fff96
Artem Svetlov <trolleway@yandex.ru>
2021-04-23 02:17:46 +0300

    upd

--- enc-twitter.sh
+++ enc-twitter.sh
@@ -1,8 +1,6 @@
-echo $1
-echo $2
 
 SRC=$1
-DST=$2
-ffmpeg -i $SRC -y -vf scale=1920:1080 -c:v libx264  -crf 18 -preset fast -c:a copy $DST
+DST=$(dirname $1)/$(basename $1| cut -d. -f1)-twitter.MP4
+ffmpeg -i $SRC -y -vf scale=1920:1080 -c:v libx264  -crf 19 -preset fast -c:a copy $DST
 
 # -vf scale=-1:1080
---
+++ enc.sh
@@ -0,0 +1,8 @@
+
+SRC=$1
+DST=$(dirname $1)/$(basename $1| cut -d. -f1)-twitter.MP4
+ffmpeg -i $SRC -y -vf scale=1920:1080 -c:v libx264 -hide_banner -tune film -crf 19 -preset medium -c:a copy $DST
+
+
+DST=$(dirname $1)/$(basename $1| cut -d. -f1)-instagram.MP4
+ffmpeg -i $SRC -y -vf crop=2160:2160:653:0  -vf scale=1920:1080 -c:v libx264  -framerate 30  -crf 19 -preset faster -c:a copy $DST
