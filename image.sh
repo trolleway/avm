c95750388e218491ea5885aefd9859c0fc9d2cd0
Artem Svetlov <trolleway@yandex.ru>
2021-04-30 03:07:04 +0300

    Update image.sh

--- image.sh
+++ image.sh
@@ -1,5 +1,8 @@
 # Convert image to 3-second 2160p60 video
 
-SRC=$1
-DST=$(dirname $1)/$(basename $1| cut -d. -f1)-video.mp4
-ffmpeg -y -loop 1 -i $SRC -c:v libx265 -t 3 -pix_fmt yuv420p -vf scale=3840:2160 -framerate 60 $DST
+SRC=$1 #folder
+for f in $1/*.mp4
+do
+    DST=$(dirname $f)/$(basename $f| cut -d. -f1)-video.mp4
+    ffmpeg -y -loop 1 -i $f -c:v libx265 -t 3 -pix_fmt yuv420p -vf scale=3840:2160 -framerate 60 $DST
+done
