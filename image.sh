923fa8ff897a71c6efdcc601da0e15a82e830c5e
trolleway <trolleway@yandex.ru>
2021-04-30 03:04:41 +0300

    upd

--- image.sh
+++ image.sh
@@ -2,4 +2,4 @@
 
 SRC=$1
 DST=$(dirname $1)/$(basename $1| cut -d. -f1)-video.mp4
-ffmpeg -y -loop 1 -i SRC -c:v libx265 -t 3 -pix_fmt yuv420p -vf scale=3840:2160 -framerate 60 $DST
+ffmpeg -y -loop 1 -i $SRC -c:v libx265 -t 3 -pix_fmt yuv420p -vf scale=3840:2160 -framerate 60 $DST
