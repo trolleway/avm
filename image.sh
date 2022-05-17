25ecef57a1ab18db14afe11595cac6b72968775a
Artem Svetlov <trolleway@yandex.ru>
2022-05-17 17:43:23 +0300

    Add files via upload

--- image.sh
+++ image.sh
@@ -6,8 +6,11 @@ do
     DST=$(dirname $f)/$(basename $f| cut -d. -f1)-video.mp4
     ffmpeg -y -loop 1 -i $f -c:v libx265 \
 -t 3 -pix_fmt yuv420p \
--vf scale=w=3840:h=2160:force_original_aspect_ratio=disable \
--framerate 60 -filter:v fps=60 \
--vf "fade=t=out:st=2.5:d=0.5" \
+-vf \
+"scale=w=3840:h=2160:force_original_aspect_ratio=disable, \
+fade=t=out:st=2.5:d=0.5, \
+fps=60" \
+-framerate 60  \
 $DST
 done
+##-vf "fade=t=out:st=2.5:d=0.5" \
