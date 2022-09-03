98fb5661e53411f776b02e2e1f0ef2c149ddc62f
trolleway <trolleway@yandex.ru>
2022-09-03 11:05:37 +0300

    48fpsslowmo

---
+++ h265to48.sh
@@ -0,0 +1,21 @@
+
+# convert 120fps to 60fps for h265
+# $2 - start time
+# $3 - duration time
+
+SRC=$1
+DST=$(dirname $1)/$(basename $1| cut -d. -f1)-48fps.mp4
+
+
+
+if [ ! -z $2 ] 
+then 
+    ffmpeg -hide_banner -loglevel error -i $SRC  -ss $2 -t $3  -y -map 0:v -c:v copy -bsf:v  hevc_mp4toannexb raw.h265
+	ffmpeg -hide_banner -loglevel error -fflags +genpts -r 48  -y -i raw.h265 -c:v copy $DST
+	rm  raw.h265
+else
+    ffmpeg -hide_banner -loglevel error -i $SRC  -y -map 0:v -c:v copy -bsf:v  hevc_mp4toannexb raw.h265
+	ffmpeg -hide_banner -loglevel error -fflags +genpts -r 48  -y -i raw.h265 -c:v copy $DST
+	rm  raw.h265
+fi
+
