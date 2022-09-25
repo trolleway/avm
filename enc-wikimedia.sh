14895670c7ccf55b5a15adf6bf8e7f742a591a7a
Artem Svetlov <trolleway@yandex.ru>
2022-09-25 22:44:21 +0300

    Update enc-wikimedia.sh

--- enc-wikimedia.sh
+++ enc-wikimedia.sh
@@ -9,5 +9,4 @@ if [[ -z "$1" ]] ;then echo "Input file missing" ;exit ;fi
 input="$1"
 output=$(basename "$1").webm
 
-ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -row-mt 1 -an -f webm -y /dev/null &&
-ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -row-mt 1 -c:a libopus "$output"
+ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -row-mt 1 -an -f webm -y /dev/null && ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -row-mt 1 -c:a libopus "$output"
