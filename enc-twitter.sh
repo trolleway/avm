e5c74521d62d822ed5cfc220189ea50293f3d9f9
Artem Svetlov <trolleway@yandex.ru>
2023-12-10 00:15:11 +0300

    Update enc-twitter.sh

--- enc-twitter.sh
+++ enc-twitter.sh
@@ -1,6 +1,6 @@
 
 SRC=$1
 DST=$(dirname $1)/$(basename $1| cut -d. -f1)-twitter.mp4
-ffmpeg -i $SRC -y -vf scale=1920:1080 -c:v libx264  -crf 19 -preset fast -c:a copy $DST
+ffmpeg -i $SRC -y -vf scale=1920:1080 -c:v libx264  -crf 24 -preset fast -c:a copy $DST
 
 # -vf scale=-1:1080
