c340ee7ee23eb6e914c09ec35db58c7d6aeeecb1
Artem Svetlov <trolleway@yandex.ru>
2023-12-10 00:16:33 +0300

    Update enc-wikimedia.sh

--- enc-wikimedia.sh
+++ enc-wikimedia.sh
@@ -12,4 +12,4 @@ output=$(dirname $1)/$(basename $1| cut -d. -f1).webm
 echo "Frames count:"
 ffprobe -v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0 $input
 
-ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -row-mt 1 -an -f webm -y /dev/null && ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -row-mt 1 -c:a libopus "$output"
+ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -row-mt 1 -an -f webm -y /dev/null && ffmpeg -i "$input" -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -row-mt 1 -c:a libopus  -movflags use_metadata_tags  "$output"
