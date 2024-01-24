3220fb8a2be2b4213ec05dd36aef8fe1426d3ea2
Artem Svetlov <trolleway@yandex.ru>
2024-01-24 22:16:29 +0300

    fps

---
+++ fps_interpolate.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+import csv, os, shutil, argparse, errno,subprocess
+import glob
+
+def argparser_prepare():
+
+    class PrettyFormatter(argparse.ArgumentDefaultsHelpFormatter,
+        argparse.RawDescriptionHelpFormatter):
+
+        max_help_position = 35
+
+    parser = argparse.ArgumentParser(description='change video speed by change fps withouth recompress',
+            formatter_class=PrettyFormatter)
+    parser.add_argument( 'path', type=str, 
+                        help='path to file')    
+    parser.add_argument( 'fps', type=int, 
+                        help='new fps')
+
+    return parser
+    
+
+def determine_codec(filename):
+    command = ['ffprobe', '-show_format', '-pretty', '-show_entries', 'stream=codec_name', '-loglevel', 'quiet', filename]
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err =  p.communicate()
+    ffmpeg_result = out.decode()
+
+    if 'codec_name=hevc' in ffmpeg_result:
+        return 'h265'
+    if 'codec_name=mjpeg' in ffmpeg_result:
+        return 'mjpeg'
+    return 'h264'
+    
+parser = argparser_prepare()
+args = parser.parse_args()
+src = args.path
+fps = args.fps
+
+#dst=$(dirname $1)/$(basename $1| cut -d. -f1)-48fps.mp4
+
+temp_mp4 = os.path.splitext(src)[0]+'-'+'temp'+'.mp4'
+dst = os.path.splitext(src)[0]+'-'+str(fps)+'.webm'
+
+if not(os.path.isfile(src)):
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), src)
+    
+
+source_codec = determine_codec(src)
+
+
+if source_codec == 'mjpeg':
+    cmd = '''
+    ffmpeg -hide_banner -loglevel error -i {src} -y -filter:v "minterpolate='mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60'" -c:v libx264 -crf 0 -preset ultrafast  {temp_mp4}
+    
+ffmpeg -hide_banner -loglevel error -i {temp_mp4} -vf scale=-1:720 -c:v libvpx-vp9 -b:v 0 -crf 27 -row-mt 1 -pix_fmt yuv420p -c:a libopus  -movflags use_metadata_tags {dst}
+
+'''
+    cmd = cmd.format(src=src, dst=dst, fps=fps,temp_mp4=temp_mp4)
+    print(cmd)
+    os.system(cmd)
+    os.unlink(temp_mp4)
+    
+if source_codec == 'h265':
+    cmd = '''
+ffmpeg -hide_banner -loglevel error -i {src}  -y -map 0:v -c:v copy -bsf:v  hevc_mp4toannexb raw.h265
+	ffmpeg -hide_banner -loglevel error -fflags +genpts -r {fps}  -y -i raw.h265 -c:v copy {dst}
+
+'''
+    cmd = cmd.format(src=src, dst=dst, fps=fps)
+    os.system(cmd)
+    os.unlink('raw.h265')
+    
+elif source_codec == 'h264':
+    cmd = '''
+    ffmpeg -hide_banner -loglevel error -i {src}  -y -map 0:v -c:v copy -bsf:v  h264_mp4toannexb  raw.h264
+	ffmpeg -fflags +genpts -r {fps}  -y -i raw.h264 -c:v copy {dst}
+'''
+    cmd = cmd.format(src=src, dst=dst, fps=fps)
+    os.system(cmd)
+    os.unlink('raw.h264')
+    
+    
+    
---
+++ merge_alpha.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+import csv, os, shutil, argparse, errno,subprocess
+import glob
+
+def argparser_prepare():
+
+    class PrettyFormatter(argparse.ArgumentDefaultsHelpFormatter,
+        argparse.RawDescriptionHelpFormatter):
+
+        max_help_position = 35
+
+    parser = argparse.ArgumentParser(description='merge video in folder without recompress',
+            formatter_class=PrettyFormatter)
+    parser.add_argument( 'path', type=str, 
+                        help='path to folder')    
+
+
+    return parser
+    
+
+def determine_codec(filename):
+    command = ['ffprobe', '-show_format', '-pretty', '-show_entries', 'stream=codec_name', '-loglevel', 'quiet', filename]
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err =  p.communicate()
+    ffmpeg_result = out.decode()
+
+    if 'codec_name=hevc' in ffmpeg_result:
+        return 'h265'
+    if 'codec_name=mjpeg' in ffmpeg_result:
+        return 'mjpeg'
+    return 'h264'
+    
+def get_dir_filenames(path,ext)->list:
+
+    files=list()
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.lower().endswith(ext.lower()):
+                files.append(os.path.join(root, file))
+    return files
+
+def list2textfile(items:list,filepath:str):
+    with open(filepath, "w") as file:
+        for item in items:
+            file.write('file '+item)
+                 
+parser = argparser_prepare()
+args = parser.parse_args()
+folder = args.path
+
+if not(os.path.isdir(folder)):
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), folder)
+files = get_dir_filenames(folder,'.avi')
+assert len(files)>0
+
+print(files)
+source_codec = determine_codec(files[0])
+listfile='list.txt'
+list2textfile(files,listfile)
+
+dstfilebase = os.path.splitext(files[0])[0]+'-merge'
+
+
+
+
+if source_codec == 'mjpeg':
+    dst=dstfilebase+'.avi'
+    cmd = ['ffmpeg','-f','concat','safe',0,'-i',listfile,'-c','copy',dst]
+    print(' '.join(cmd))
+    subprocess.run(cmd)
+    quit()
+    os.unlink(listfile)
+    print('finished')
+    print(dst)
+    
+    
+else:
+    '''
+    #create dir if not exists
+    mkdir -p  $DST
+    #convert to mpeg transport stream
+    for f in $1/*.[mM][pPKk][4vV]; do ffmpeg -y  -hide_banner -loglevel error -i $f -c copy  -f mpegts $DST/$(basename $f| cut -d. -f1).ts; done
+    LIST='concat:'
+    for f in $DST/*.ts; do LIST+="$f|" ; done
+    #merge
+    ffmpeg -y  -hide_banner -loglevel error -i "$LIST" -c copy -bsf:a aac_adtstoasc $DST/$DATE.mp4
+
+    rm -rf $DST/*.ts
+    rm -rf $DST/list.txt
+    
+    '''
+
+
+    
+    
