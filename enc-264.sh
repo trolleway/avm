4e32ecec0b2522ca54687f9458a07329dadd97fd
Artem Svetlov <trolleway@yandex.ru>
2024-02-05 18:11:21 +0300

    audio to aac

--- enc-264.sh
+++ enc-264.sh
@@ -1,3 +1,3 @@
 SRC=$1
 DST=$(dirname $1)/$(basename $1| cut -d. -f1)-h264.mp4
-ffmpeg -i $SRC -hide_banner -y -c:v libx264  -crf 19 -preset slow -tune film -c:a copy $DST
+ffmpeg -i $SRC -hide_banner -y -c:v libx264  -crf 19 -preset slow -tune film -c:a aac $DST
