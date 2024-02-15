bbe1e267842c5c7c5c464de23e5011af3172d007
trolleway <trolleway@yandex.ru>
2024-02-15 19:19:11 +0300

    enc-stories

--- enc-stories.sh
+++ enc-stories.sh
@@ -9,27 +9,27 @@ DST=$(dirname $1)/$(basename $1| cut -d. -f1)-stories.mp4
 
 POS=$2
 
-ffplay -filter:v "crop=ih/16*9:ih" ~/tmp/skot.mp4
+#ffplay -filter:v "crop=ih/16*9:ih" ~/tmp/skot.mp4
 
 case $POS in
 
   1)
-    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih:0:0" -c:v libx264  -crf 19 -preset fast -c:a copy $DST
+    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih:0:0" -c:v libx264  -crf 19 -preset fast $DST
     ;;
 
   2)
-    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih:iw/10" -c:v libx264  -crf 19 -preset fast -c:a copy $DST
+    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih:iw/10" -c:v libx264  -crf 19 -preset fast  $DST
     ;;
 
   3)
-    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih" -c:v libx264  -crf 19 -preset fast -c:a copy $DST
+    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih" -c:v libx264  -crf 19 -preset fast  $DST
     ;;
 
   *)
-    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih" -c:v libx264  -crf 19 -preset fast -c:a copy $DST
+    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih" -c:v libx264  -crf 19 -preset fast $DST
     ;;
 
   9)
-    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih:iw" -c:v libx264  -crf 19 -preset fast -c:a copy $DST
+    ffmpeg -stats -i $SRC -y -vf scale=1080:1920 -vf "crop=ih/16*9:ih:iw" -c:v libx264  -crf 19 -preset fast  $DST
     ;;
 esac
