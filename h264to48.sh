5c9b90deecf7e417c4ae6bf01f816ace05cc6526
Artem Svetlov <trolleway@yandex.ru>
2025-07-21 04:02:39 +0300

    h264to48

---
+++ h264to48.sh
@@ -0,0 +1,21 @@
+
+# convert 120fps to 60fps for h264
+# $2 - start time
+# $3 - duration time
+
+SRC=$1
+DST=$(dirname $1)/$(basename $1| cut -d. -f1)-48fps.mp4
+
+
+
+if [ ! -z $2 ] 
+then 
+    ffmpeg -i $SRC  -ss $2 -t $3  -y -map 0:v -c:v copy -bsf:v  h264_mp4toannexb  raw.h264
+	ffmpeg -fflags +genpts -r 48  -y -i raw.h264 -c:v copy $DST
+	rm  raw.h264
+else
+    ffmpeg -i $SRC  -y -map 0:v -c:v copy -bsf:v  h264_mp4toannexb  raw.h264
+	ffmpeg -fflags +genpts -r 48  -y -i raw.h264 -c:v copy $DST
+	rm  raw.h264
+fi
+
